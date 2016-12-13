51d63da15d1b2a936ff1468c3bd893cc7e863aae
Your Name <you@example.com>
2016-12-13 19:27:15 +0900

    2번째

---
+++ Ex4_polymorphism/src/Poly.java
@@ -0,0 +1,15 @@
+
+public class Poly {
+	
+	public static void makeSoundTest(Animal animal){
+		animal.makeSound();
+		
+	}
+	public static void main(String[] args) {
+		Cat cat = new Cat();
+		Dog dog = new Dog();
+		
+		
+	}
+
+}
